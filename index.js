304a7c7cc9fac689ccd163783201da82f13af295
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-09-07 00:54:55 +0530

    updating

---
+++ index.js
@@ -0,0 +1,25 @@
+var util = require('util');
+var express = require('express');
+var builder = require('component-middleware');
+var cors = require('cors');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(cors());
+
+var domains = ['accounts', 'autos'];
+
+domains.forEach(function (domain) {
+    app.use(util.format('/%s/master/%s', domain, domain), express.static(domain));
+
+    app.get(util.format('/%s/*', domain), builder({
+        root: process.cwd() + '/' + domain,
+        path: util.format('/%s/master/index', domain)
+    }));
+});
+
+app.listen(process.env.PORT, function () {
+    console.log('cdn app listening on port %s', process.env.PORT);
+});
