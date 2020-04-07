e15300d9af47ffa877335dc3b5c77f1e31de980b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-04-07 14:39:45 +0530

    updating

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ app.use(express.static('public'));
 
 var domains = ['accounts', 'admin', 'autos', 'www', 'realestates'];
 
+app.get('/facebook/token', function (req, res) {
+    res.json({
+        access_token: '123456'
+    });
+});
+
+app.get('/facebook/profile', function (req, res) {
+    res.json({
+        email: 'ruchira.wageesha@gmail.com',
+        first_name: 'Ruchira',
+        last_name: 'Wageesha'
+    });
+});
+
+app.get('/facebook/dialog/oauth', function (req, res) {
+    var uri = 'http://development.accounts.serandives.com:4000/auth/oauth?code=123456';
+    res.redirect(uri);
+});
+
 domains.forEach(function (domain) {
     app.use(util.format('/%s/master/%s', domain, domain), express.static(domain));
 
